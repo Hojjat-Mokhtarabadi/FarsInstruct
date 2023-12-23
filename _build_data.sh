f3f8a1b8edea8636ca1469b76eadc3cacd68287c
Hojjat Mokhtarabadi <hojjatmokhtari.1999@gmail.com>
2023-12-23 15:30:33 +0330

    result dir fixed

--- _build_data.sh
+++ _build_data.sh
@@ -1,8 +1,8 @@
 cd FarsInstruct
 
-python build_data_gym/build_gym.py --split validation \
+python build_data_gym/build_gym.py --split test \
                                    --ds_name PNLPhub/FarsTail \
                                    --generate_metadata \
-                                   --prompt_format llama
+                                   --prompt_format hooshvare
 
 cd ..
--- _run_evaluation.sh
+++ _run_evaluation.sh
@@ -1,5 +1,6 @@
 cd FarsInstruct
 
+# accelerate launch evaluation/run_eval.py --task_type multiple_choice
 python evaluation/run_eval.py --task_type multiple_choice
 
 cd ..
