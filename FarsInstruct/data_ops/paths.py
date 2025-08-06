d72dcee1420ae2a3584de1b4a531d0e734cd5962
Dr.Manshaei <hmokhtarabadi.1999@gmail.com>
2025-08-06 14:07:23 +0330

    sni dataset init commit

--- FarsInstruct/data_ops/paths.py
+++
@@ -1,4 +0,0 @@
-DATA_FILES = {
-    'train': f"data/instruct_dataset_train.csv", 
-    'validation': 'data/instruct_dataset_validation.csv', 
-    'test': 'data/instruct_dataset_test.csv'}
---
+++ FarsInstruct/data_ops/sni/sni_dataset.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+from torch.utils.data import Dataset
+
+class SNIDataset(Dataset):
+    def __init__(self):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, idx):
+        pass
+
